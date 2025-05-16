a4431af597f6d93f805f50f4db3c195f11f7efaf
KingDreemur <107367492+KingDreemur@users.noreply.github.com>
2025-05-16 20:15:02 -0300

    16/05/2025
    Ainda Adicionar o:
    Desafios para os alunos
    • Criar uma função para filtrar alimentos com data de validade próxima.
    • Exibir um alerta quando um alimento estiver com estoque baixo.
    • Criar categorias de alimentos (frutas, legumes, carnes, etc.).

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('titulo')"Minha Aplicação"</title>
+    <link rel="stylesheet">
+</head>
+<body>
+    
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
